9618e29654097ab088a68af90005c2d51f93ad06
LeyLopez <leidislopez2017@gmail.com>
2023-09-09 11:06:03 -0500

    changes

---
+++ ClassLanguages.py
@@ -0,0 +1,13 @@
+from ABC import SuperClass
+from ClassAlphabet import Alphabet
+
+class Languages(SuperClass):
+
+    def union(self):
+        pass
+
+    def interception(self):
+        pass
+
+    def difference(self):
+        pass
