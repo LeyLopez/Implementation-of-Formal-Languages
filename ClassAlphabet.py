19076b36ece5d5ee67a806fa4ea3202dbebb4a1b
LeyLopez <leidislopez2017@gmail.com>
2023-09-09 10:55:37 -0500

    news

--- ClassAlphabet.py
+++ ClassAlphabet.py
@@ -6,21 +6,21 @@ class Alphabet(SuperClass):
     self.alphabet2=set()
 
   def enter_alphabets(self):
-    alphabet_one=input("Enter A: {")
+    alphabet_one=input("Enter A: ")
     alphabet_two= input("B: ")
 
     self.alphabet1 = set(alphabet_one.split(', '))
     self.alphabet2 = set(alphabet_two.split(', '))
 
   def show(self):
-    print(f"Enter A: {{{', '.join(self.alphabet1)}}} B: {{{', '.join(self.alphabet2)}}}")   
+    print(f"A: {{{', '.join(self.alphabet1)}}} B: {{{', '.join(self.alphabet2)}}}")   
 
   def union(self):
     pass
 
   def interception(self):
     pass
-
+  
   def difference(self):
     pass    
     
---
+++ Main.py
@@ -0,0 +1,10 @@
+from ClassAlphabet import Alphabet
+from ABC import SuperClass
+
+def main():
+    my_alphabet=Alphabet()
+
+    my_alphabet.enter_alphabets()
+    my_alphabet.show()
+
+main()
