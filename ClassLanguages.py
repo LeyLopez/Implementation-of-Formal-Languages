74a450379a47cc72935b2753b5590f0ed2cb4d64
mcamilamp <camimerpa@gmail.com>
2023-09-10 12:17:36 -0500

    Ready the enter_num_words

--- ClassLanguages.py
+++ ClassLanguages.py
@@ -1,7 +1,17 @@
 from ABC import SuperClass
 from ClassAlphabet import Alphabet
 
-class Languages(SuperClass):
+class Languages(SuperClass): 
+
+    def __init__(self):
+        self.language1 = set()
+        self.language2 = set()
+        self.alphabet = Alphabet
+
+    def enter_num_words(self):
+        num_words_one = int(input("Enter the number of words in Language A: "))
+        num_words_two = int(input("Enter the number of words in Language B: "))
+        
 
     def union(self):
         pass
@@ -11,3 +21,7 @@ class Languages(SuperClass):
 
     def difference(self):
         pass
+
+
+my_language = Languages()
+my_language.enter_num_words()  
